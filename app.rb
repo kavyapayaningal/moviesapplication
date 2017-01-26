52187052b34569d89ebf13e5e31e9707dc646c4b
matsue-city10 <kavyapayaningal@gmail.com>
2017-01-26 17:05:06 +0900

    presentation version

--- app.rb
+++ app.rb
@@ -4,6 +4,11 @@ require 'data_mapper'
 
 require_relative 'models/init'
 
+get '/login' do
+
+    erb :login
+end
+
   get '/' do
   @movies = Movie.all
   erb :index
@@ -17,6 +22,11 @@ end
 get '/movies/:id/update' do
   @movies =Movie.all
   erb :update
+end
+
+get '/movies/:id/download' do
+  @movies =Movie.all
+  erb :download
 end
 
 
